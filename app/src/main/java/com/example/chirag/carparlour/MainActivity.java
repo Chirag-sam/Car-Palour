d2c5e1f1ac8666b413196a603f6bac225a1ad7e1
Chirag-sam <chiraggsam@gmail.com>
2017-01-31 07:21:07 +0530

    Navigation drawer

--- app/src/main/java/com/example/chirag/carparlour/MainActivity.java
+++ app/src/main/java/com/example/chirag/carparlour/MainActivity.java
@@ -1,16 +1,28 @@
 package com.example.chirag.carparlour;
 
-import android.app.ActionBar;
-import android.content.Intent;
-import android.graphics.Color;
-import android.graphics.drawable.ColorDrawable;
-import android.support.v7.app.AppCompatActivity;
-import android.os.Bundle;
-import android.view.View;
-import android.widget.ArrayAdapter;
-import android.widget.Button;
+        import android.app.ActionBar;
+        import android.content.Intent;
+        import android.graphics.Color;
+        import android.graphics.drawable.ColorDrawable;
+        import android.net.Uri;
+        import android.support.design.widget.NavigationView;
+        import android.support.v4.print.PrintHelper;
+        import android.support.v4.widget.DrawerLayout;
+        import android.support.v7.app.ActionBarDrawerToggle;
+        import android.support.v7.app.AppCompatActivity;
+        import android.os.Bundle;
+        import android.support.v7.widget.Toolbar;
+        import android.view.MenuItem;
+        import android.view.View;
+        import android.widget.ArrayAdapter;
+        import android.widget.Button;
+        import android.view.Menu;
+        import android.widget.Toast;
+
 
 public class MainActivity extends AppCompatActivity {
+    private DrawerLayout dl;
+    private ActionBarDrawerToggle abdt;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -22,6 +34,42 @@ public class MainActivity extends AppCompatActivity {
         Button offers = (Button)findViewById(R.id.offers);
         Button support = (Button)findViewById(R.id.support);
         Button comp = (Button)findViewById(R.id.comp);
+
+
+        dl = (DrawerLayout)findViewById(R.id.drawerlayout);
+        abdt = new ActionBarDrawerToggle(this, dl , R.string.Open, R.string.Close);
+
+        dl.addDrawerListener(abdt);
+        abdt.syncState();
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+
+        NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
+
+        navigationView.setNavigationItemSelectedListener(new NavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(MenuItem menuItem) {
+                int id = menuItem.getItemId();
+                if (id == R.id.nav_account) {
+
+                    Toast.makeText(MainActivity.this, "My account",
+                            Toast.LENGTH_LONG).show();
+
+                } else if (id == R.id.my_sub) {
+                    Toast.makeText(MainActivity.this, "My sub",
+                            Toast.LENGTH_LONG).show();
+
+                } else if (id == R.id.t_c) {
+                    Toast.makeText(MainActivity.this, "terms and cond",
+                            Toast.LENGTH_LONG).show();
+                }
+                else if (id == R.id.change_pass) {
+                    Toast.makeText(MainActivity.this, "change pass",
+                            Toast.LENGTH_LONG).show();
+            }
+                return true;
+            }
+        });
+
 
 
 
@@ -68,4 +116,12 @@ public class MainActivity extends AppCompatActivity {
 
     }
 
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        if(abdt.onOptionsItemSelected(item))
+        {return true;}
+
+
+        return super.onOptionsItemSelected(item);
+    }
 }
